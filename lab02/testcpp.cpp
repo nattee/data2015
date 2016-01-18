d3abf00956eb0e8bbd0e596918d82c0064bd7eca
Nattee Niparnan <nattee@gmail.com>
2016-01-18 22:03:44 +0700

    lab02

---
+++ lab02/testcpp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+  int n;
+  cin >> n;
+  cout << "Please enter " << n << "integers." << endl;
+  //create your vector
+  for (int i= 0;i < n;i++) {
+    int val;
+    cin  >> val;
+
+  }
+
+  //reverse it
+
+}
+
