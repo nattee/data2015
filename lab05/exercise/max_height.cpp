8c3f89d35a04fa6ca0e25565a4c5300e9dbd93e2
Nattee Niparnan <nattee@gmail.com>
2016-01-27 07:28:20 +0700

    ex

---
+++ lab05/exercise/max_height.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int n;
+  cout << "Enter number of people: ";
+  cin >> n;
+  for (int i = 0;i < n;i++) {
+    float height;
+    string name;
+    cin >> height >> name;
+    //for example 1.75 Nattee
+    //for example 1.80 Abc
+  }
+
+  int k;
+  cout << "Enter number of highest people: ";
+  cin >> k;
+  //using MAP or priority_queue to find highest K people
+  //write your code here
+}
+
