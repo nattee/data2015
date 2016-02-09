3bb5872145628356bb68bf20f90bbad3bfd4e7ed
Nattee Niparnan <nattee@gmail.com>
2016-02-09 14:46:43 +0700

    move files

--- lab02/exercise/reverse.cpp
+++ lab02/exercise/reverse.cpp
@@ -9,14 +9,18 @@ int main() {
   int n;
   cin >> n;
   cout << "Please enter " << n << "integers." << endl;
-  //create your vector
+  //create your vector here
   for (int i= 0;i < n;i++) {
     int val;
     cin  >> val;
-
   }
 
-  //reverse it
+
+
+
+  //show what you have reverse
+  //the result is a vector (not necessary the same variable as the original one) that
+  //contains element in a reverse order given by the user.
 
 }
 
