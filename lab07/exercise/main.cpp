27476211d0448e179fc733bfa8b7f9b95ab838e6
Nattee Niparnan <nattee@gmail.com>
2016-02-15 09:49:01 +0700

    main for triplet

--- lab07/exercise/main.cpp
+++ lab07/exercise/main.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
-#include "pair.h"
+#include "triplet.h"
 
 using namespace std;
 
 int main()
 {
     cout << endl << "using default constructor" << endl;
-    CP::pair<int,int> a;
-    cout << "by default, a = " << a.first << "," << a.second << endl;
-    a.first = 1; a.second = 2;
-    cout << "now a = " << a.first << "," << a.second << endl;
+    CP::triplet<int,int,int> a;
+    cout << "by default, a = " << a.first << "," << a.second << "," << a.third << endl;
+    a.first = 1; a.second = 2; a.third = 10;
+    cout << "now, a = " << a.first << "," << a.second << "," << a.third << endl;
 
     cout << endl << "using copy constructor" << endl;
-    CP::pair<int,int> b(a);
-    cout << "b is copied from a\nb = " << b.first << "," << b.second << endl;
+    CP::triplet<int,int,int> b(a);
+    cout << "b is copied from a\nb = " << b.first << "," << b.second << "," << b.third << endl;
 
     cout << endl << "using custom constructor" << endl;
-    CP::pair<int,int> c(200,-94);
-    cout << "c = " << c.first << "," << c.second << endl;
+    CP::triplet<int,int> c(200,-94,200);
+    cout << "c = " << c.first << "," << c.second << "," << c.third << endl;
 
     cout << endl << "using another custom constructor" << endl;
-    CP::pair<int,string> d("somchai");
-    cout << "d = " << d.first << "," << d.second << endl;
+    CP::triplet<int,string,float> d("somchai");
+    cout << "d = " << d.first << "," << d.second << e"," << d.third << ndl;
 
 }
--- lab07/exercise/main_overload.cpp
+++ lab07/exercise/main_overload.cpp
@@ -1,39 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include "pair.h"
+#include "triplet.h"
 
 using namespace std;
 
 int main() {
-  CP::pair<int,std::string> a(10,"vishnu");
-  CP::pair<int,std::string> b(a);
+  CP::triplet<int,std::string,float> a(10,"vishnu",590.2);
+  CP::triplet<int,std::string,float> b(a);
 
   cout << endl << "using equality operator" << endl;
   cout << "a == b? " << (a == b ? "YES" : "NO")  << endl;
 
-  CP::pair<int,std::string> c(999,"asdf");
+  CP::triplet<int,std::string,float> c(999,"asdf",300.3);
 
   cout << endl << "using assignment  operator" << endl;
   c = a;
   cout << "a == c? " << (a == c ? "YES" : "NO")  << endl;
 
-  c.second = "abc";
+  c.third = 42.24;
   cout << "a == c? " << (a == c ? "YES" : "NO")  << endl;
 
   cout << endl << "using comparison  operator" << endl;
-  std::vector<CP::pair<int,string> > v;
-  v.push_back( CP::pair<int,string>(10,"asdf"));
-  v.push_back( CP::pair<int,string>(10,"ddd"));
-  v.push_back( CP::pair<int,string>(5,"asdf"));
-  v.push_back( CP::pair<int,string>(3,"X"));
-  v.push_back( CP::pair<int,string>(1,"asdf"));
-  v.push_back( CP::pair<int,string>(10,"zzz"));
+  std::vector<CP::triplet<int,string,float> > v;
+  v.push_back( CP::triplet<int,string,float>(10,"asdf",3.4));
+  v.push_back( CP::triplet<int,string,float>(10,"ddd",3.4));
+  v.push_back( CP::triplet<int,string,float>(5,"asdf",3.4));
+  v.push_back( CP::triplet<int,string,float>(5,"asdf",200.4));
+  v.push_back( CP::triplet<int,string,float>(3,"X",3.4));
+  v.push_back( CP::triplet<int,string,float>(1,"asdf",3.4));
+  v.push_back( CP::triplet<int,string,float>(10,"zzz",3.4));
 
   sort(v.begin(),v.end());
 
   for (size_t i = 0;i < v.size();i++) {
-    cout << v[i].first << " " << v[i].second << endl;
+    cout << v[i].first << " " << v[i].second << " " << v[i].third << endl;
   }
 }
 
