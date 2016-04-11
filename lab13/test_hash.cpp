ce55678d95463e0bc22d4107ea43aa40a1cec17a
Nattee Niparnan <nattee@gmail.com>
2016-04-11 09:30:23 +0700

    list exercise

--- lab13/test_hash.cpp
+++ lab13/test_hash.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include "unordered_map.h"
 
 using namespace std;
 
 int main() {
-  map<string,int> mm;
+  CP::unordered_map<string,int> mm;
 
   mm["somchai"] = 10;
   mm["nuttapong"] = 20;
 
 
-  cout << mm["attawit"] << endl;
+  cout << mm["attawith"] << endl;
   cout << mm["somchai"] << endl;
   cout << mm["nuttapong"] << endl;
   cout << mm["athasit"] << endl;
   cout << mm["123"] << endl;
   cout << mm["zzz"] << endl;
 
-  auto it = mm.begin();
-  cout << it->first << endl; it++;
-  cout << it->first << endl; it++;
-  cout << it->first << endl; it++;
-  cout << it->first << endl; it++;
-  cout << it->first << endl; it++;
+  cout << "-------------- done ------------" << endl;
+  cout << "attawith = " <<  mm["attawith"] << endl;
+  auto xx = mm.insert(make_pair("uuuiuiouou",30));
+  cout << "xx.first.first = " << xx.first->first << endl;
+  cout << "xx.first.second = " << xx.first->second << endl;
+  cout << "xx.second = " << xx.second << endl;
+
 
 }
 
